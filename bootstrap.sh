0e76e6703f0c02f70460a8ba25012db368adf780
Jater <1452606865@qq.com>
2021-09-23 15:34:59 +0800

    Add Fastlane

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+echo "checking for homebrew updates";
+
+brew update
+
+function install_current {
+  echo "try to update $1"
+  brew upgrade $1 || brew install $1 || true
+  brew link $1
+}
+
+if [ -e "Gemfile" ]; then
+  echo "installing ruby gems";
+  gem install bundler --no-document || echo "failed to install bundle";
+  bundle config set deployment "true";
+  bundle config path vendor/bundle;
+  bundle install || echo "Failed to install bundle";
+if
