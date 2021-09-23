b452c0f64e0f9858a88752f65d01e9b5c68900a5
Jater <1452606865@qq.com>
2021-09-23 16:17:47 +0800

    Fix: Fastlane simulator error

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ function install_current {
 
 if [ -e "Gemfile" ]; then
   echo "installing ruby gems";
-  gem install bundler || echo "failed to install bundle";
+  gem install bundler --no-document || echo "failed to install bundle";
   bundle config set deployment 'true';
   bundle config path vendor/bundle;
   bundle install --jobs 4 --retry 3 || echo "failed to install bundle";
